77fa573736a1d2d3056c80e0e66bdab8afa3f7af
mattroz <matveyrozanov@gmail.com>
2017-08-22 11:51:09 +0300

    Implemented decision border plotting using perceptron weights

--- classifiers/test.py
+++ classifiers/test.py
@@ -18,22 +18,29 @@ X = iris_df.iloc[:100, [sepal_idx, petal_idx]].values
 y = iris_df.iloc[:100, class_idx].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 
-# Add setosa data to the plot
-plt.scatter(X[:50, 0], X[:50, 1], marker='o', color='red', label='setosa')
-# Add versicolor data to the plot
-plt.scatter(X[50:, 0], X[50:, 1], marker='x', color='blue', label='versicolor')
-# Add labels to axis
-plt.xlabel('sepal length')
-plt.ylabel('petal length')
-plt.legend(loc='upper left')
-plt.show()
-
 # Train perceptron
 perceptron = Perceptron()
 perceptron.fit(X, y)
 
 # Plot perceptron error progress through epochs
+plt.subplot(2,1,1)
 plt.plot(range(1, len(perceptron.errors) + 1), perceptron.errors, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Num of missclassif-s')
+
+# Plot data and decision border
+plt.subplot(2,1,2)
+plt.scatter(X[:50, 0], X[:50, 1], marker='o', color='red', label='setosa')
+plt.scatter(X[50:, 0], X[50:, 1], marker='x', color='blue', label='versicolor')
+plt.xlabel('sepal length')
+plt.ylabel('petal length')
+plt.legend(loc='upper left')
+
+# Decision border is a line w0 + w1*x + w2*y = 0,
+# Hence y = -w1/w2*x - w0/w2
+w = perceptron.weights
+x_ = X[:,0]
+y_ = (-w[1]/w[2] * x_) - (w[0]/w[2])
+plt.plot(x_, y_, color='black')
 plt.show()
+
