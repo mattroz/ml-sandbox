e82a247b78c83b0c232d870e99447070e88b5392
mattroz <matveyrozanov@gmail.com>
2017-08-24 23:07:23 +0300

    Changed weight_ to weights_

--- classifiers/adaline/adaline_classifier.py
+++ classifiers/adaline/adaline_classifier.py
@@ -5,15 +5,11 @@ class Adaline:
 	def __init__(self, learning_rate=0.1, n_iters=10):
 		self.learning_rate = learning_rate
 		self.n_iters = n_iters
-	
-	
-	def net_input(self, X):
-		return (self.weights_[0] + np.dot(X, self.weights_[1:]))
 
 
 	def fit(self, X, y):
 		n_features = X.shape[1]
-		self.weight_ = np.random.randn(n_features + 1)
+		self.weights_ = np.random.randn(n_features + 1)
 		self.cost_ = []
 		# Start to iterate through epochs
 		for _ in range(self.n_iters):
@@ -21,11 +17,15 @@ class Adaline:
 			# Weights update implemented using gradient descent,
 			# where cost function is J(w) = SSE: 
 			# 1/2 * sum(y - sum(w[j]*x[i][j])^2
-			self.weight_[1:] += self.learning_rate * X.T.dot(errors)
-			self.weight_[0] += self.learning_rate * errors.sum()
+			self.weights_[1:] += self.learning_rate * X.T.dot(errors)
+			self.weights_[0] += self.learning_rate * errors.sum()
 			cost = (errors**2).sum() / 2.0
 			self.cost_.append(cost)
 		return self
+	
+	
+	def net_input(self, X):
+		return (self.weights_[0] + np.dot(X, self.weights_[1:]))
 
 	
 	def activation(self, X):
