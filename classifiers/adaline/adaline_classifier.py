6c91c130d258db42cfd33d52cbcd6983c22a1eb1
mattroz <matveyrozanov@gmail.com>
2017-08-24 22:18:19 +0300

    Small fix about weights

--- classifiers/adaline/adaline_classifier.py
+++ classifiers/adaline/adaline_classifier.py
@@ -13,7 +13,7 @@ class Adaline:
 
 	def fit(self, X, y):
 		n_features = X.shape[1]
-		self.weights_ = np.random.randn(n_features + 1)
+		self.weight_ = np.random.randn(n_features + 1)
 		self.cost_ = []
 		# Start to iterate through epochs
 		for _ in range(self.n_iters):
@@ -22,7 +22,7 @@ class Adaline:
 			# where cost function is J(w) = SSE: 
 			# 1/2 * sum(y - sum(w[j]*x[i][j])^2
 			self.weight_[1:] += self.learning_rate * X.T.dot(errors)
-			self.weights[0] += self.learning_rate * errors.sum()
+			self.weight_[0] += self.learning_rate * errors.sum()
 			cost = (errors**2).sum() / 2.0
 			self.cost_.append(cost)
 		return self
