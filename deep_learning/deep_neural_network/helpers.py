ef8b81a7500b6baec12de9bb8fac83f5d2e1cd81
mattroz <matveyrozanov@gmail.com>
2018-03-29 13:54:18 +0300

    Moved predict() function to the notebook

--- deep_learning/deep_neural_network/helpers.py
+++ deep_learning/deep_neural_network/helpers.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import h5py
 
-mport numpy as np
+import numpy as np
 
 def relu(Z):
 	return np.maximum(0, Z)
@@ -32,40 +32,6 @@ def load_data():
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-
-def predict(X, y, parameters):
-    """
-    This function is used to predict the results of a  L-layer neural network.
-    
-    Arguments:
-    X -- data set of examples you would like to label
-    parameters -- parameters of the trained model
-    
-    Returns:
-    p -- predictions for the given dataset X
-    """
-    
-    m = X.shape[1]
-    n = len(parameters) // 2 # number of layers in the neural network
-    p = np.zeros((1,m))
-    
-    # Forward propagation
-    probas, caches = L_model_forward(X, parameters)
-
-    
-    # convert probas to 0/1 predictions
-    for i in range(0, probas.shape[1]):
-        if probas[0,i] > 0.5:
-            p[0,i] = 1
-        else:
-            p[0,i] = 0
-    
-    #print results
-    #print ("predictions: " + str(p))
-    #print ("true labels: " + str(y))
-    print("Accuracy: "  + str(np.sum((p == y)/m)))
-        
-    return p
 
 def print_mislabeled_images(classes, X, y, p):
     """
